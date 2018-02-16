fec2dc0087d51069e26c3c1e5a3cd43999e86518
mirekrousal@centrum.cz <mirekrousal@centrum.cz>
2018-02-16 16:28:45 +0000

    Library loader as separate file

--- vars/SDLC_NODE.groovy
+++ vars/SDLC_NODE.groovy
@@ -2,44 +2,10 @@ def call(nodeName, librariesPaths, body) {
     def libs = [:]
     
 	stage("Initialize libraries") {
-		node('master') {
-			for (path in librariesPaths) {
-				libs = loadAllFiles(createFilePath(path), path, libs)
-			}
-		}
+		libs = libraryLoader.loadLibraries(librariesPaths)
 	}
 
 	node(nodeName) {
 		body(libs)
 	}
-}
-
-def getLibraries() {
-	return libs
-}
-
-def loadAllFiles(rootPath, path, libs) {
-    for (subPath in rootPath.list()) {
-        fileName = subPath.getBaseName()
-        if (!subPath.isDirectory()) {
-           echo "loading library ${fileName}"
- 	       
- 	       loadedLib = load "${path}/${fileName}.groovy"
-          
-           libs[fileName] = loadedLib
-	    }
-    }
-
-    return libs
-}
-
-// Helps if slave servers are in picture
-def createFilePath(def path) {
-    echo path
-    if (env['NODE_NAME'].equals("master")) {
-        File localPath = new File(path)
-        return new hudson.FilePath(localPath);
-    } else {
-        return new hudson.FilePath(Jenkins.getInstance().getComputer(env['NODE_NAME']).getChannel(), path);
-    }
 }
---
+++ vars/libraryLoader.groovy
@@ -0,0 +1,35 @@
+def createFilePath(def path) {
+    echo path
+    if (env['NODE_NAME'].equals("master")) {
+        File localPath = new File(path)
+        return new hudson.FilePath(localPath);
+    } else {
+        return new hudson.FilePath(Jenkins.getInstance().getComputer(env['NODE_NAME']).getChannel(), path);
+    }
+}
+
+def loadAllFiles(rootPath, path, libs) {
+    for (subPath in rootPath.list()) {
+        fileName = subPath.getBaseName()
+        if (!subPath.isDirectory()) {
+           echo "loading library ${fileName}"
+ 	       
+ 	       loadedLib = load "${path}/${fileName}.groovy"
+          
+           libs[fileName] = loadedLib
+	    }
+    }
+
+    return libs
+}
+
+def loadLibraries(librariesPaths) {
+	def libs = [:]
+	node('master') {
+		for (path in librariesPaths) {
+			libs = loadAllFiles(createFilePath(path), path, libs)
+		}
+	}
+
+	return libs
+}
